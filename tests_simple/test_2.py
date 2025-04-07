8cc3228e6a140d61ce47e0fa86d9dc85df7539c9
Blazzer87 <nitros1987@gmail.com>
2025-04-07 17:35:03 +0300

    workPC commit

--- tests_simple/test_2.py
+++ tests_simple/test_2.py
@@ -1,14 +1,46 @@
+import re
+
+import pytest
 from playwright.sync_api import Playwright, sync_playwright, expect
 
+@pytest.fixture(autouse=True, scope="function", params=["chromium", "firefox", "webkit"])
+def page2(request, playwright: Playwright):
 
-def test_add_todo(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
-    page.goto("https://demo.playwright.dev/todomvc/#/")
-    page.get_by_placeholder("What needs to be done?").click()
-    page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
-    page.get_by_placeholder("What needs to be done?").press("Enter")
 
-    context.close()
-    browser.close()
+        browser_type = request.param
+
+        browser = getattr(playwright, browser_type).launch(headless=False, slow_mo=500, timeout=60000)
+
+        # создает изолированный сеанс браузера (новый объект окна браузера)
+        context = browser.new_context()
+
+        # создает страницу браузера в рамках этого изолированного сеанса
+        page2 = context.new_page()
+
+        yield page2
+
+        context.close()
+        browser.close()
+
+
+def test_2(page2):
+
+        # передаёт в страницу какой урл нам необходимо открыть
+        page2.goto("https://demoqa.com/text-box", wait_until='domcontentloaded')
+        # где wait_until='domcontentloaded' - один из вариантов ожидания ранней загрузки страницы
+
+        # действие передачи текста по объекту который найден через РОЛЬ
+        page2.get_by_role("textbox", name="Full Name").fill("Sergey")
+
+        # действие передачи текста по объекту который найден через ЛОКАТОР
+        page2.locator("#permanentAddress").fill("и тут тоже какой то адрес")
+
+        # действие передачи нажатия клавиши в объект который найден через ЛОКАТОР
+        page2.locator("#permanentAddress").press("Enter")
+
+
+        page2.get_by_role("textbox", name="Current Address").fill("павапвап")
+        page2.locator("#permanentAddress").fill("павп")
+        page2.get_by_role("textbox", name="Full Name").fill("апыапывап")
+        page2.get_by_role("textbox", name="name@example.com").fill("sdfgsdfgdsf")
+
